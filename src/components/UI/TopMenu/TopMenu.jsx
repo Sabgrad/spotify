1cd68a0872a851f05f526a46425c9dc3a06391d8
Andrjuha-joma-jozhik <andrey.zhmudenko1@gmail.com>
2022-12-25 03:30:10 +0200

    Update TopMenu.jsx

--- src/components/UI/TopMenu/TopMenu.jsx
+++ src/components/UI/TopMenu/TopMenu.jsx
@@ -5,11 +5,9 @@ import MyButton from "../MyButton/MyButton";
 
 const TopMenu = () => {
 
-    const CLIENT_ID = 'CLIENT_ID ';
     const REDIRECT_URI = 'http://localhost:3000';
     const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
     const RESPONSE_TYPE = 'token';
-    const CLIENT_SECRET = 'CLIENT_SECRET';
     const SCOPE = 'streaming%20ugc-image-upload%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20app-remote-control%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-private%20playlist-modify-public%20user-follow-modify%20user-follow-read%20user-read-playback-position%20user-top-read%20user-read-recently-played%20user-library-modify%20user-library-read%20user-read-email%20user-read-private'
 
     const {token, setToken} = useContext(AuthContext);
