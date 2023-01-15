8e5ef5b45a868e1f7c105afb7749701b17df0e4e
Andrjuha-joma-jozhik <andrey.zhmudenko1@gmail.com>
2023-01-15 04:44:40 +0200

    Update common.js

--- src/common/common.js
+++ src/common/common.js
@@ -1,6 +1,10 @@
 export const SCOPE = 'streaming%20ugc-image-upload%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20app-remote-control%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-private%20playlist-modify-public%20user-follow-modify%20user-follow-read%20user-read-playback-position%20user-top-read%20user-read-recently-played%20user-library-modify%20user-library-read%20user-read-email%20user-read-private';
+export const CLIENT_ID = 'client id';
 export const REDIRECT_URI = 'http://localhost:3000';
-export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+export const AUTH_ENDPOINT_CODE = 'https://accounts.spotify.com/authorize';
+export const AUTH_ENDPOINT_TOKEN = 'https://accounts.spotify.com/api/token';
 export const REQUEST_ENDPOINT = 'https://api.spotify.com/v1/';
 export const RESPONSE_TYPE = 'code';
+export const CLIENT_SECRET = 'client secret';
 export const MARKET = 'UA';
+export const BEARER = 'Bearer ';
